0aff55bf19000f72b5e686d061e7726e70de44fe
Niclas Prock <niclas.prock@elmatrix.de>
2025-07-21 17:35:17 +0200

    FEATURE: added annotation level forwarding support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
-## 0.1.0
+## 0.3.0
+- Added annotation-level forwarding support
 
+## 0.1.0
 -  `@Decorator()` class annotation
--- lib/src/decorator.dart
+++ lib/src/decorator.dart
@@ -1,3 +1,15 @@
 class Decorator {
-  const Decorator();
+  const Decorator({
+    this.forwardToString,
+    this.forwardEquals,
+    this.forwardHashCode,
+    this.forwardRuntimeType,
+    this.forwardNoSuchMethod,
+  });
+
+  final bool? forwardToString;
+  final bool? forwardEquals;
+  final bool? forwardHashCode;
+  final bool? forwardRuntimeType;
+  final bool? forwardNoSuchMethod;
 }
